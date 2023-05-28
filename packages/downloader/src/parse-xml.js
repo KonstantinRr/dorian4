c96403a73e77781d35a7c18a4abe7d649a2e82d8
Alex Procelewski <33789063+alexthe2@users.noreply.github.com>
2023-05-28 09:51:04 +0200

    Update parse-xml.js

--- packages/downloader/src/parse-xml.js
+++ packages/downloader/src/parse-xml.js
@@ -8,10 +8,11 @@ module.exports = async function parseXml(data){
 	$(`loc`).each((_, el) => {
 		const loc = $(el)
 		console.log(`Loc: "${loc}"`) 
-		const url = loc.text().strip()
+		const url_o = loc.text()
 		console.log(`Url parsed: "${url}"`)
-		if(url){
+		if(url_o){
 			console.log(`Doing the other stuff now`)
+			const url = url_o.trim()
 			this.addToQueue(url)
 			const newUrl = this.convertUrl(url)
 			loc.html(newUrl)
